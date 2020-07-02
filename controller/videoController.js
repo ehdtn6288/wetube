2cd8a596ffef59efeb34aec6850eedd481e1baa5
ehdtn6288 <43023496+ehdtn6288@users.noreply.github.com>
2020-07-03 01:11:50 +0900

    searchController

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,14 +1,25 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  console.log(searchingBy);
+  //const searchingBy = req.query.term;
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
 
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
+
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "VideoDetil" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "editVideo" });
+
 export const deleteVideo = (req, res) =>
   res.render("Delete Video", { pageTitle: "DeleteVideo" });
