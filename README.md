5c61cc8ba8662a9d0c096eb88feac34b52ae4b15
ehdtn6288 <43023496+ehdtn6288@users.noreply.github.com>
2020-07-03 18:15:51 +0900

    videoBlock with mixin/modifier other pages

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Cloning Youtube with Vanilla and NodeJS
 
 ## Pages
 
-- [ ] Home
+- [x] Home
 - [x] Join
 - [x] Log In
 - [x] Search
 - [ ] User Detail
-- [ ] Edit Profile
-- [ ] Change Password
-- [ ] Upload
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
 - [ ] Video Detail
-- [ ] Edit Video
+- [x] Edit Video
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 11111,
+    title: "Video Magnifique",
+    description: "This is somthing I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1333029,
+      name: "UDS",
+      email: "dsu6288@naver.com",
+    },
+  },
+  {
+    id: 22222,
+    title: "Video Fantastique",
+    description: "This is somthing I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1333029,
+      name: "UDS",
+      email: "dsu6288@naver.com",
+    },
+  },
+  {
+    id: 33333,
+    title: "Video Incontrounable",
+    description: "This is somthing I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1333029,
+      name: "UDS",
+      email: "dsu6288@naver.com",
+    },
+  },
+  {
+    id: 44444,
+    title: "Video Parfait",
+    description: "This is somthing I love",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 1333029,
+      name: "UDS",
+      email: "dsu6288@naver.com",
+    },
+  },
+];
