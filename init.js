2797fa34f6a0f17cd52edcb690e0063399bac895
ehdtn6288 <43023496+ehdtn6288@users.noreply.github.com>
2020-07-15 01:09:53 +0900

    UserSchema

--- init.js
+++ init.js
@@ -3,6 +3,9 @@ import app from "./app";
 import dotenv from "dotenv";
 import "./models/video";
 import "./models/comment";
+import Comment from "./models/comment";
+import User from "./models/user";
+import Video from "./models/video";
 
 dotenv.config();
 const PORT = process.env.PORT | 4000;
