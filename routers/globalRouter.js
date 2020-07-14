442bbd95f41f89a0c16279006b7c4b4476c6ac51
ehdtn6288 <43023496+ehdtn6288@users.noreply.github.com>
2020-07-15 03:25:58 +0900

    limits the acces to the pages according to the state of login

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -8,19 +8,20 @@ import {
   getLogin,
   postLogin,
 } from "../controller/userController";
+import { onlyPublic, onlyPrivate } from "../middlewares";
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 
 //join
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
 //login
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPublic, logout);
 
 export default globalRouter;
