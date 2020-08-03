cac151b086587b72788e7de2196d2ae2dd4c8f3e
ehdtn6288 <43023496+ehdtn6288@users.noreply.github.com>
2020-08-03 09:58:44 +0900

    s

--- src/init.js
+++ src/init.js
@@ -7,9 +7,9 @@ import "./models/comment";
 import "./models/user";
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = 4000;
 
 const handleListening = () =>
-  console.log(`✅ Listening on: http://localhost:${PORT}`);
+  console.log(`✅  Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
